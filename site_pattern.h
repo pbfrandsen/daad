3d5ddff63acb895efe27499facb23b4301c2d2cc
Paul Frandsen <paul.frandsen@rutgers.edu>
2014-07-09 14:08:49 +0200

    Added SitePattern class with print function

--- site_pattern.h
+++ site_pattern.h
@@ -12,9 +12,36 @@
 #include <vector>
 #include "faststring2.h"
 
-class site_pattern
+class SitePattern
 {
+    faststring pattern;
+public:
+    // Takes a faststring that conatins the site pattern
+    SitePattern(faststring new_pattern)
+    {
+        pattern = new_pattern;
+    }
     
+    unsigned number_of_sites = pattern.length();
+    vector<bitset<number_of_sites> > bitset_vector;
+    
+//    void calculate_pa(friend SitePattern j)
+//    {
+//        
+//    }
+    
+    // print to the stream that the user chooses
+    void print(std::ostream &os)
+    {
+        os << pattern;
+    }
+    
+    // fill an array with 4 bitsets
+    
+    void gen_bitset_vector()
+    {
+        
+    }
 };
 
 #endif
